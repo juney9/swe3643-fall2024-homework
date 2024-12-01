d286b0940ce4e98656d15a8e95b8bd80c20a309f
juney9 <juneyhk91@gmail.com>
2024-12-01 16:12:12 -0500

    completed hw 1 tutorial

---
+++ Homework1/TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Homework1;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            ClassicAssert.AreEqual(4, Calculator.Add(2,2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            ClassicAssert.AreEqual(4, Calculator.Add(2,2));
+        }
+    }
+}
